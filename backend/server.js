aa4f20aee3151c2df5a3c429998e2068fafffa4a
DarshK25 <darshkalathiya25@gmail.com>
2025-01-17 19:20:32 +0530

    Updating

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,30 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './config/db.js';
-import path from 'path';
-import url from 'url';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+import { connectDB } from "./config/db.js";
+
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
-app.use(express.json()); // To allow json data to be sent in the request body
-//Send data along with the request   
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.use('/api/products', productRoutes)
+app.use("/api/products", productRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
-  // Adjust the path if necessary
-  
-    // For any request that doesn't match an API route, serve the React app
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));  // Adjust the path if necessary
-    });
-  }
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
-app.listen(port, ()=> {
-    connectDB();
-    console.log(`Server is running on port: ${port}`);
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
 });
