e1c797af260cb3b19602ece9549d57ca562ecf74
DarshK25 <darshkalathiya25@gmail.com>
2025-01-17 20:30:57 +0530

    updating

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'frontend/dist',  // Output directory for production
+    outDir: 'dist',  // Output directory for production
   },
   server: {
     proxy: {
